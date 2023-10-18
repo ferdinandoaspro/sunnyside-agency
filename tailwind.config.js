285da3919268147e930a2d1d6426144b5145947d
FatChickn <ferdinandoaspro2@gmail.com>
2023-10-18 16:48:47 +0200

    added background image support

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,28 @@ module.exports = {
         "dark-grayish-blue": "hsl(232, 10%, 55%)",
         "grayish-blue": "hsl(210, 4%, 67%)",
         "white": "hsl(0, 0%, 100%)"
+      },
+      backgroundImage: {
+        //mobile backgrounds//
+        "cone": "url('/images/mobile/image-gallery-cone.jpg')",
+        "milkbottles": "url('/images/mobile/image-gallery-milkbottles.jpg')",
+        "orange": "url('/images/mobile/image-gallery-orange.jpg')",
+        "sugar-cubes": "url('/images/mobile/image-gallery-sugar-cubes.jpg')",
+        "graphic-design": "url('/images/mobile/image-graphic-design.jpg')",
+        "header": "url('/images/mobile/image-header.jpg')",
+        "photography": "url('/images/mobile/image-photography.jpg')",
+        "stand-out": "url('/images/mobile/image-stand-out.jpg')",
+        "transform": "url('/images/mobile/image-transform.jpg')",
+        //desktop backgrounds//
+        "cone-desktop": "url('/images/desktop/image-gallery-cone.jpg')",
+        "milkbottles-desktop": "url('/images/desktop/image-gallery-milkbottles.jpg')",
+        "orange-desktop": "url('/images/desktop/image-gallery-orange.jpg')",
+        "sugar-cubes-desktop": "url('/images/desktop/image-gallery-sugar-cubes.jpg')",
+        "graphic-design-desktop": "url('/images/desktop/image-graphic-design.jpg')",
+        "header-desktop": "url('/images/desktop/image-header.jpg')",
+        "photography": "url('/images/desktop/image-photography.jpg')",
+        "stand-out": "url('/images/desktop/image-stand-out.jpg')",
+        "transform": "url('/images/desktop/image-transform.jpg')"
       }
     },
   },
